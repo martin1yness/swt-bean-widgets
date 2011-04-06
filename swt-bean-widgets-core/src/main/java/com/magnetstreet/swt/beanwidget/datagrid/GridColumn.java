2d78c3e34a5f267647b9337ff1e31fc0eb51c6ab
Martin Dale Lyness <martin.lyness@gmail.com>
2011-04-06 13:30:49 -0500

    Moved to using the getter method to retrieve properties to work with proxy objects

--- swt-bean-widgets-core/src/main/java/com/magnetstreet/swt/beanwidget/datagrid/GridColumn.java
+++ swt-bean-widgets-core/src/main/java/com/magnetstreet/swt/beanwidget/datagrid/GridColumn.java
@@ -5,6 +5,7 @@ import com.magnetstreet.swt.util.DateUtil;
 import com.magnetstreet.swt.util.TableColumnComparator;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.math.BigDecimal;
 import java.util.Calendar;
 
@@ -18,6 +19,7 @@ import java.util.Calendar;
 public class GridColumn<T> {
     public String title = "";
     public Field beanProperty = null;
+    public Method beanPropertyGetter = null;
     public int widthHint = 40;
     public TableColumnComparator sortAlgorithm = null;
     public boolean editable = true;
@@ -26,20 +28,26 @@ public class GridColumn<T> {
     public GridColumn(String title, Field beanProperty, int widthHint) {
         this.title = title;
         this.beanProperty = beanProperty;
+        try {
+            this.beanPropertyGetter = beanProperty.getDeclaringClass().getMethod("get"+Character.toUpperCase(beanProperty.getName().charAt(0))+beanProperty.getName().substring(1));
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException("Given bean property '"+beanProperty.getName()+"' does not have a tradtional getter.");
+        }
         this.widthHint = widthHint;
     }
 
     public Object getDisplayValue(T bean) {
         try {
-            beanProperty.setAccessible(true);
+            beanPropertyGetter.setAccessible(true);
+            Object propVal = beanPropertyGetter.invoke(bean);
             if(beanProperty.getType() == Calendar.class)
-                return DateUtil.getDefaultDateTimeFormatted((Calendar)beanProperty.get(bean));
+                return DateUtil.getDefaultDateTimeFormatted((Calendar)propVal);
             if(beanProperty.getType() == BigDecimal.class)
-                return (((BigDecimal)beanProperty.get(bean))==null) ? "" : ((BigDecimal)beanProperty.get(bean)).setScale(2, BigDecimal.ROUND_HALF_UP).toString();
+                return (propVal==null) ? "" : ((BigDecimal)propVal).setScale(2, BigDecimal.ROUND_HALF_UP).toString();
             else
-                return beanProperty.get(bean);
+                return propVal;
         } catch(Throwable e) {
-            throw new InvalidGridViewSetupException("Getter for member name '"+beanProperty.getName()+"' of column '"+title +"' could not be dynamically loaded.", e);
+            throw new InvalidGridViewSetupException("Getter '"+beanPropertyGetter.getName()+"' for member name '"+beanProperty.getName()+"' of column '"+title +"' could not be dynamically loaded.", e);
         }
     }
 }

d9f056f9d9c054ede77d0fade9110e56312dae6d
Martin Dale Lyness <martin.lyness@gmail.com>
2011-05-02 14:59:02 -0500

    Updated DateTimePopout to use a better positioning function as well as allow programatic popout action with atomic library

--- swt-bean-widgets-core/src/main/java/com/magnetstreet/swt/beanwidget/datetime/DateTimePopout.java
+++ swt-bean-widgets-core/src/main/java/com/magnetstreet/swt/beanwidget/datetime/DateTimePopout.java
@@ -12,10 +12,12 @@ import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.DateTime;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Logger;
 
 /**
@@ -29,7 +31,7 @@ import java.util.logging.Logger;
  * @since Oct 26, 2009
  */
 public class DateTimePopout extends DateTime {
-    public static volatile boolean popoutOpen = false;
+    public static AtomicBoolean popoutOpen = new AtomicBoolean(false);
     private Logger logger = Logger.getLogger(DateTimePopout.class.getSimpleName());
     private long lastClicked = 0;
 
@@ -52,53 +54,82 @@ public class DateTimePopout extends DateTime {
 
     @Override protected void checkSubclass() {}
 
-    public void applyPopoutListener() {
+    public void showPopout() {
+        if(popoutOpen.getAndSet(true)) {
+            logger.warning("Datetime widget is already popped out, it will not honor this request to open the calendar view again until the other popout is closed.");
+            return;
+        }
+        final Shell dialog = new Shell (getShell(), SWT.ON_TOP);
+        dialog.setLocation(getXCoordPosition(), getYCoordPosition());
+        dialog.setLayout (new FormLayout());
+
+        final DateTime calendar = new DateTime (dialog, SWT.CALENDAR);
+        DateTimeUtil.copyDateTime(getSelf(), calendar);
+        FormData calendarLData = new FormData();
+        calendarLData.left = new FormAttachment(0,100,0);
+        calendarLData.right = new FormAttachment(100,100,0);
+        calendarLData.top = new FormAttachment(0,100,0);
+        calendarLData.bottom = new FormAttachment(100,100,-25);
+        calendar.setLayoutData(calendarLData);
+        final DateTime time = new DateTime (dialog, SWT.TIME);
+        DateTimeUtil.copyDateTime(getSelf(), time);
+        FormData timeLData = new FormData(90,20);
+        timeLData.left = new FormAttachment(0,100,0);
+        timeLData.bottom = new FormAttachment(100,100,0);
+        time.setLayoutData(timeLData);
+
+        Button ok = new Button (dialog, SWT.PUSH);
+        ok.setText ("OK");
+        FormData okLData = new FormData(50,25);
+        okLData.right = new FormAttachment(100,100,0);
+        okLData.bottom = new FormAttachment(100,100,0);
+        ok.setLayoutData(okLData);
+        ok.addSelectionListener (new SelectionAdapter() {
+            public void widgetSelected (SelectionEvent e) {
+                DateTimeUtil.copyDateTime(calendar, getSelf());
+                DateTimeUtil.copyDateTime(time, getSelf());
+                popoutOpen.set(false);
+                dialog.close ();
+            }
+        });
+        dialog.setDefaultButton (ok);
+        dialog.pack ();
+        dialog.open ();
+    }
+
+    protected int getXCoordPosition() {
+        int x = getLocation().x;
+        Control parent = getParent();
+        while(parent != null) {
+            x += parent.getLocation().x;
+            if(parent instanceof Shell) // Relative in Shell position Bug
+                break;
+            parent = parent.getParent();
+        }
+
+        return x + getBounds().width;
+    }
+
+    protected int getYCoordPosition() {
+        int y = getLocation().y;
+        Control parent = getParent();
+        while(parent != null) {
+            y += parent.getLocation().y;
+            if(parent instanceof Shell) // Relative in Shell position Bug
+                break;
+            parent = parent.getParent();
+        }
+
+        return y + 2 * getBounds().height;
+    }
+
+    protected void applyPopoutListener() {
         this.addMouseListener(new MouseAdapter(){
             @Override public void mouseUp(MouseEvent mouseEvent) {
                 long dbl = System.currentTimeMillis() - lastClicked;
                 lastClicked = System.currentTimeMillis();
                 if(dbl > 600) return;
-                if(popoutOpen) {
-                    logger.warning("Datetime widget is already popped out, it will not honor this request to open the calendar view again until the other popout is closed.");
-                    return;
-                }
-                popoutOpen = true;
-                final Shell dialog = new Shell (getShell(), SWT.ON_TOP);
-                dialog.setLocation(getLocation().x+getShell().getLocation().x, getLocation().y+getShell().getLocation().y);
-                dialog.setLayout (new FormLayout());
-
-                final DateTime calendar = new DateTime (dialog, SWT.CALENDAR);
-                DateTimeUtil.copyDateTime(getSelf(), calendar);
-                FormData calendarLData = new FormData();
-                calendarLData.left = new FormAttachment(0,100,0);
-                calendarLData.right = new FormAttachment(100,100,0);
-                calendarLData.top = new FormAttachment(0,100,0);
-                calendarLData.bottom = new FormAttachment(100,100,-25);
-                calendar.setLayoutData(calendarLData);
-                final DateTime time = new DateTime (dialog, SWT.TIME);
-                DateTimeUtil.copyDateTime(getSelf(), time);
-                FormData timeLData = new FormData(90,20);
-                timeLData.left = new FormAttachment(0,100,0);
-                timeLData.bottom = new FormAttachment(100,100,0);
-                time.setLayoutData(timeLData);
-
-                Button ok = new Button (dialog, SWT.PUSH);
-                ok.setText ("OK");
-                FormData okLData = new FormData(50,25);
-                okLData.right = new FormAttachment(100,100,0);
-                okLData.bottom = new FormAttachment(100,100,0);
-                ok.setLayoutData(okLData);
-                ok.addSelectionListener (new SelectionAdapter() {
-                    public void widgetSelected (SelectionEvent e) {
-                        DateTimeUtil.copyDateTime(calendar, getSelf());
-                        DateTimeUtil.copyDateTime(time, getSelf());
-                        popoutOpen = false;
-                        dialog.close ();
-                    }
-                });
-                dialog.setDefaultButton (ok);
-                dialog.pack ();
-                dialog.open ();
+                showPopout();
             }
         });
     }

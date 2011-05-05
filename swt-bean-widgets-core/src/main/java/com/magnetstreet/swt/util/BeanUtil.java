ffcad2d0c8085caabf324191e50f71a2fcb07868
Martin Dale Lyness <martin.lyness@gmail.com>
2011-05-05 12:25:16 -0500

    Updated bean util to use the isPropertyName convention with getters on boolean values

--- swt-bean-widgets-core/src/main/java/com/magnetstreet/swt/util/BeanUtil.java
+++ swt-bean-widgets-core/src/main/java/com/magnetstreet/swt/util/BeanUtil.java
@@ -70,49 +70,56 @@ public class BeanUtil {
         if(chain.length == 1)
             return getFieldValueWithGetter(baseObject, chain[0]);
 
-        return getFieldChainValueWithGetters(getFieldValueWithGetter(baseObject, chain[0]), fieldNameChain.substring(chain[0].length()+1));
+        return getFieldChainValueWithGetters(getFieldValueWithGetter(baseObject, chain[0]), fieldNameChain.substring(chain[0].length() + 1));
     }
 
     public static Object getFieldValueWithGetter(Object obj, Field field) {
-        return getFieldValueWithGetter(obj, field.getName());
-    }
-
-    public static Object getFieldValueWithGetter(Object obj, String fieldName) {
         try {
-            Method m = getGetterMethodForField(obj, fieldName);
+            Method m = getGetterMethodForField(obj, field);
             m.setAccessible(true);
             return m.invoke(obj);
         } catch (NoSuchMethodException e) {
-            log.log(Level.INFO, "No getter for field: "+ fieldName +", using field it's self", e);
+            log.log(Level.INFO, "No getter for field: "+ field.getName() +", using field it's self", e);
             try {
-                Field field = obj.getClass().getDeclaredField(fieldName);
                 field.setAccessible(true);
                 return field.get(obj);
             } catch(Throwable iae) {
                 log.log(Level.SEVERE,"Unable to access field even after enabling access.", iae);
             }
         } catch (InvocationTargetException e) {
-            log.log(Level.SEVERE,"Unable to invoke getter on supplied object: " + obj +", field: " + fieldName, e);
+            log.log(Level.SEVERE,"Unable to invoke getter on supplied object: " + obj +", field: " + field.getName(), e);
         } catch (IllegalAccessException e) {
             log.log(Level.SEVERE,"Access not allowed on explicitly marked accessible field through reflection.", e);
         }
         return null;
     }
 
-    public static Method getGetterMethodForField(Object obj, Field field) throws NoSuchMethodException {
-        return obj.getClass().getDeclaredMethod(getGetterMethodNameForField(obj, field));
+    public static Object getFieldValueWithGetter(Object obj, String fieldName) {
+        try {
+            return getFieldValueWithGetter(obj, obj.getClass().getDeclaredField(fieldName));
+        } catch (NoSuchFieldException e) {
+            log.log(Level.SEVERE, "Unable to find field on object", e);
+            return null;
+        }
     }
 
-    public static Method getGetterMethodForField(Object obj, String fieldName) throws NoSuchMethodException {
-        return obj.getClass().getDeclaredMethod(getGetterMethodNameForField(fieldName));
+    public static Method getGetterMethodForField(Object obj, Field field) throws NoSuchMethodException {
+        try {
+            return obj.getClass().getDeclaredMethod(getGetterMethodNameForField(obj, field));
+        } catch(NoSuchMethodException nsme) {
+            log.log(Level.WARNING, "Can't find method.", nsme);
+            throw nsme;
+        }
+    }
+
+    public static Method getGetterMethodForField(Object obj, String fieldName) throws NoSuchMethodException, NoSuchFieldException {
+        return obj.getClass().getDeclaredMethod(getGetterMethodNameForField(fieldName, obj.getClass().getDeclaredField(fieldName)));
     }
 
     public static String getGetterMethodNameForField(Object obj, Field field) {
-        return getGetterMethodNameForField(field.getName());
-    }
-
-    public static String getGetterMethodNameForField(String fieldName) {
-        return "get"+fieldName.substring(0, 1).toUpperCase()+fieldName.substring(1);
+        if(field.getType() == Boolean.class || field.getType() == boolean.class)
+            return "is"+field.getName().substring(0, 1).toUpperCase()+field.getName().substring(1);
+        return "get"+field.getName().substring(0, 1).toUpperCase()+field.getName().substring(1);
     }
 
     public static String getSetterMethodNameForField(String fieldName) {

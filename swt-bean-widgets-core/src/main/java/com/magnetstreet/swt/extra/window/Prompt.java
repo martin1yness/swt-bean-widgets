ce819e1cd97ea68fb1b90f22bf33f85e0c92f6f2
Martin Dale Lyness <martin.lyness@gmail.com>
2011-09-23 13:20:19 -0500

    Updated Prompt specialized window as it was never really working.

--- swt-bean-widgets-core/src/main/java/com/magnetstreet/swt/extra/window/Prompt.java
+++ swt-bean-widgets-core/src/main/java/com/magnetstreet/swt/extra/window/Prompt.java
@@ -2,12 +2,10 @@ package com.magnetstreet.swt.extra.window;
 
 import com.magnetstreet.swt.extra.VisualInputText;
 import org.eclipse.jface.dialogs.Dialog;
-import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
@@ -21,21 +19,32 @@ import org.eclipse.swt.widgets.Shell;
  * @since Sep 21, 2010
  */
 public class Prompt extends Dialog {
-    private String message;
+    private String message, title;
     private Label messageLabel;
     private VisualInputText userInputText;
-    private Button okButton, cancelButton;
+
+    private String input;
 
     public Prompt(Shell parent, String title, String msg) {
         super(parent);
         this.message = msg;
-        getShell().setText(title);
+        this.title = title;
     }
 
-    protected Control createDialogArea(Composite parent) {
-        parent.setLayout(new FormLayout());
+    @Override public boolean close() {
+        input = userInputText.getText();
+        return super.close();
+    }
+
+    @Override protected void createButtonsForButtonBar(Composite parent) {
+        super.createButtonsForButtonBar(parent);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    @Override protected Control createDialogArea(Composite parent) {
+        Composite container = new Composite(parent, SWT.EMBEDDED);
+        container.setLayout(new FormLayout());
         {
-            messageLabel = new Label(parent, SWT.NONE);
+            messageLabel = new Label(container, SWT.NONE);
             FormData fd = new FormData(Math.min( (message.length()+10)*5, Display.getDefault().getPrimaryMonitor().getBounds().width-10), 25);
             fd.height = 25;
             fd.left = new FormAttachment(0, 100, 2);
@@ -44,18 +53,12 @@ public class Prompt extends Dialog {
             messageLabel.setLayoutData(fd);
         }
         {
-            userInputText = new VisualInputText(parent, SWT.BORDER);
+            userInputText = new VisualInputText(container, SWT.BORDER);
             FormData fd = new FormData(Math.min( (message.length()+10)*5, Display.getDefault().getPrimaryMonitor().getBounds().x-10), 25);
             fd.left = new FormAttachment(0, 100, 2);
             fd.right = new FormAttachment(100,100,-2);
             fd.top = new FormAttachment(0,100,45);
             userInputText.setLayoutData(fd);
-        }
-        {
-            createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, false);
-        }
-        {
-            createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, true);
         }
         messageLabel.setText(message);
         userInputText.setDefaultText("Quick access key 'SHIFT + ?'");
@@ -63,10 +66,11 @@ public class Prompt extends Dialog {
         userInputText.setAutoRefillDefaultText(true);
         userInputText.forceFocus();
 
-        return parent;
+        getShell().setText(title);
+        return container;
     }
 
     public String getUserInput() {
-        return userInputText.getText();
+        return input;
     }
 }

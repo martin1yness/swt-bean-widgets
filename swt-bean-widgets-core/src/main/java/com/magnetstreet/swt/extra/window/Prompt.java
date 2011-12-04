f268a308bbd839bce9d1220ba470a2a0ba40fd00
Martin Dale Lyness <martin.lyness@gmail.com>
2011-12-04 15:55:59 -0600

    UPdated prompt to accomidate inputs more accurately.

--- swt-bean-widgets-core/src/main/java/com/magnetstreet/swt/extra/window/Prompt.java
+++ swt-bean-widgets-core/src/main/java/com/magnetstreet/swt/extra/window/Prompt.java
@@ -44,20 +44,29 @@ public class Prompt extends Dialog {
         Composite container = new Composite(parent, SWT.EMBEDDED);
         container.setLayout(new FormLayout());
         {
+            String[] messageLines = message.split("\n");
+            String longestLine = "";
+            for(String s: messageLines) {
+                if(longestLine.length() < s.length())
+                    longestLine = s;
+            }
             messageLabel = new Label(container, SWT.NONE);
-            FormData fd = new FormData(Math.min( (message.length()+10)*5, Display.getDefault().getPrimaryMonitor().getBounds().width-10), 25);
-            fd.height = 25;
-            fd.left = new FormAttachment(0, 100, 2);
-            fd.right = new FormAttachment(100,100,-2);
+            FormData fd = new FormData();
+            fd.width = Math.min( (longestLine.length()+10)*5, Display.getDefault().getPrimaryMonitor().getBounds().width-10);
+            fd.height = Math.min( 15 * messageLines.length, Display.getDefault().getPrimaryMonitor().getBounds().height-10);
+            fd.left = new FormAttachment(0, 100, 10);
+            fd.right = new FormAttachment(100,100,-10);
             fd.top = new FormAttachment(0,100,10);
             messageLabel.setLayoutData(fd);
         }
         {
-            userInputText = new VisualInputText(container, SWT.BORDER);
-            FormData fd = new FormData(Math.min( (message.length()+10)*5, Display.getDefault().getPrimaryMonitor().getBounds().x-10), 25);
+            userInputText = new VisualInputText(container, SWT.BORDER|SWT.MULTI);
+            FormData fd = new FormData();
+            fd.height = 75;
             fd.left = new FormAttachment(0, 100, 2);
             fd.right = new FormAttachment(100,100,-2);
-            fd.top = new FormAttachment(0,100,45);
+            fd.top = new FormAttachment(messageLabel,45);
+            fd.bottom = new FormAttachment(100,100,0);
             userInputText.setLayoutData(fd);
         }
         messageLabel.setText(message);
